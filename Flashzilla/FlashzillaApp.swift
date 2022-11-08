6263bd64ae6114bd8fdbb2218ca8b17a4392935f
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-11-08 22:38:31 +0300

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Николай Никитин on 08.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
